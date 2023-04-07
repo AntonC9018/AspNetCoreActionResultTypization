8c1dd63fde450de61dd865c54d275f564ac5eb0d
AntonC9018 <anton.curm@gmail.com>
2023-04-07 11:25:06 +0300

    initial

---
+++ AspNetCoreActionResultTypizer/AspNetCoreActionResultTypizer.CodeFixes/MakeConstCodeFixProvider.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Simplification;
+
+namespace AspNetCoreActionResultTypizer
+{
+    using static SyntaxFactory;
+    
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AspNetCoreActionResultTypizerCodeFixProvider)), Shared]
+    public class AspNetCoreActionResultTypizerCodeFixProvider : CodeFixProvider
+    {
+        private static readonly ImmutableArray<string> _FixableDiagnosticIds = ImmutableArray.Create(AspNetCoreActionResultTypizerAnalyzer.DiagnosticId);
+        
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => _FixableDiagnosticIds;
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = (await context.Document.GetSyntaxRootAsync(context.CancellationToken))!;
+
+            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start)!.Parent!.AncestorsAndSelf().OfType<LocalDeclarationStatementSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixResources.CodeFixTitle,
+                    createChangedDocument: c => AspNetCoreActionResultTypizerAsync(context.Document, declaration, c),
+                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+        }
+
+        private static async Task<Document> AspNetCoreActionResultTypizerAsync(Document document,
+            LocalDeclarationStatementSyntax localDeclaration,
+            CancellationToken cancellationToken)
+        {
+            // Remove the leading trivia from the local declaration.
+            var firstToken = localDeclaration.GetFirstToken();
+            var leadingTrivia = firstToken.LeadingTrivia;
+            var trimmedLocal = localDeclaration.ReplaceToken(
+                firstToken, firstToken.WithLeadingTrivia(SyntaxTriviaList.Empty));
+
+            // Create a const token with the leading trivia.
+            var constToken = Token(
+                leadingTrivia, 
+                SyntaxKind.ConstKeyword, 
+                TriviaList(ElasticMarker));
+
+            // Insert the const token into the modifiers list, creating a new modifiers list.
+            var newModifiers = trimmedLocal.Modifiers.Insert(0, constToken);
+
+            // If the type of the declaration is 'var', create a new type name
+            // for the inferred type.
+            var variableDeclaration = localDeclaration.Declaration;
+            var variableTypeName = variableDeclaration.Type;
+            if (variableTypeName.IsVar)
+            {
+                var semanticModel = (await document.GetSemanticModelAsync(cancellationToken))!;
+
+                // Special case: Ensure that 'var' isn't actually an alias to another type
+                // (e.g. using var = System.String).
+                var aliasInfo = semanticModel.GetAliasInfo(variableTypeName, cancellationToken);
+                if (aliasInfo == null)
+                {
+                    // Retrieve the type inferred for var.
+                    var type = semanticModel.GetTypeInfo(variableTypeName, cancellationToken).ConvertedType!;
+
+                    // Special case: Ensure that 'var' isn't actually a type named 'var'.
+                    if (type.Name != "var")
+                    {
+                        // Create a new TypeSyntax for the inferred type. Be careful
+                        // to keep any leading and trailing trivia from the var keyword.
+                        var typeName = ParseTypeName(type.ToDisplayString())
+                            .WithLeadingTrivia(variableTypeName.GetLeadingTrivia())
+                            .WithTrailingTrivia(variableTypeName.GetTrailingTrivia());
+
+                        // Add an annotation to simplify the type name.
+                        var simplifiedTypeName = typeName.WithAdditionalAnnotations(Simplifier.Annotation);
+
+                        // Replace the type in the variable declaration.
+                        variableDeclaration = variableDeclaration.WithType(simplifiedTypeName);
+                    }
+                }
+            }
+            // Produce the new local declaration.
+            var newLocal = trimmedLocal
+                .WithModifiers(newModifiers)
+                .WithDeclaration(variableDeclaration);
+
+            // Add an annotation to format the new local declaration.
+            var formattedLocal = newLocal.WithAdditionalAnnotations(Formatter.Annotation);
+
+            // Replace the old local declaration with the new local declaration.
+            var oldRoot = (await document.GetSyntaxRootAsync(cancellationToken))!;
+            var newRoot = oldRoot.ReplaceNode(localDeclaration, formattedLocal);
+
+            // Return document with transformed tree.
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
---
+++ AspNetCoreActionResultTypizer/AspNetCoreActionResultTypizer/Analyzer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AspNetCoreActionResultTypizer;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class AspNetCoreActionResultTypizerAnalyzer : DiagnosticAnalyzer
+{
+    public const string DiagnosticId = "AspNetCoreActionResultTypizer";
+
+    // https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md
+    private static LocalizableString GetString(string name) => new LocalizableResourceString(name, Resources.ResourceManager, typeof(Resources));
+    private static readonly LocalizableString _Title = GetString(nameof(Resources.AnalyzerTitle));
+    private static readonly LocalizableString _MessageFormat = GetString(nameof(Resources.AnalyzerMessageFormat));
+    private static readonly LocalizableString _Description = GetString(nameof(Resources.AnalyzerDescription));
+    private const string _Category = "Usage";
+
+    private static readonly DiagnosticDescriptor _Rule = new DiagnosticDescriptor(DiagnosticId, _Title, _MessageFormat, _Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: _Description);
+    private static readonly ImmutableArray<DiagnosticDescriptor> _SupportedDiagnostics = ImmutableArray.Create(_Rule);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => _SupportedDiagnostics;
+
+    public override void Initialize(AnalysisContext context)
+    {
+        // https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.LocalDeclarationStatement);
+    }
+
+    private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+    {
+        var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+
+        // make sure the declaration isn't already const:
+        if (localDeclaration.Modifiers.Any(SyntaxKind.ConstKeyword))
+        {
+            return;
+        }
+
+        TypeSyntax variableTypeName = localDeclaration.Declaration.Type;
+        ITypeSymbol variableType = context.SemanticModel.GetTypeInfo(variableTypeName, context.CancellationToken).ConvertedType;
+
+        // Ensure that all variables in the local declaration have initializers that
+        // are assigned with constant values.
+        foreach (VariableDeclaratorSyntax variable in localDeclaration.Declaration.Variables)
+        {
+            EqualsValueClauseSyntax initializer = variable.Initializer;
+            if (initializer == null)
+            {
+                return;
+            }
+
+            Optional<object> constantValue = context.SemanticModel.GetConstantValue(initializer.Value, context.CancellationToken);
+            if (!constantValue.HasValue)
+            {
+                return;
+            }
+
+            // Ensure that the initializer value can be converted to the type of the
+            // local declaration without a user-defined conversion.
+            Conversion conversion = context.SemanticModel.ClassifyConversion(initializer.Value, variableType);
+            if (!conversion.Exists || conversion.IsUserDefined)
+            {
+                return;
+            }
+
+            // Special cases:
+            //  * If the constant value is a string, the type of the local declaration
+            //    must be System.String.
+            //  * If the constant value is null, the type of the local declaration must
+            //    be a reference type.
+            if (constantValue.Value is string)
+            {
+                if (variableType.SpecialType != SpecialType.System_String)
+                {
+                    return;
+                }
+            }
+            else if (variableType.IsReferenceType && constantValue.Value != null)
+            {
+                return;
+            }
+        }
+
+        // Perform data flow analysis on the local declaration.
+        DataFlowAnalysis dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(localDeclaration);
+
+        foreach (VariableDeclaratorSyntax variable in localDeclaration.Declaration.Variables)
+        {
+            // Retrieve the local symbol for each variable in the local declaration
+            // and ensure that it is not written outside of the data flow analysis region.
+            ISymbol variableSymbol = context.SemanticModel.GetDeclaredSymbol(variable, context.CancellationToken);
+            if (dataFlowAnalysis.WrittenOutside.Contains(variableSymbol))
+            {
+                return;
+            }
+        }
+
+        context.ReportDiagnostic(Diagnostic.Create(_Rule, context.Node.GetLocation()));
+    }
+}
