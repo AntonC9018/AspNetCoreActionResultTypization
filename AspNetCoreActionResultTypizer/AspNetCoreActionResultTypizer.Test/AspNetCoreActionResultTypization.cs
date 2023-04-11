038d14fd772aeab197a697067bf9da70a0b39fe4
AntonC9018 <anton.curm@gmail.com>
2023-04-11 11:37:57 +0300

    Add tests for code fix provider

--- AspNetCoreActionResultTypizer/AspNetCoreActionResultTypizer.Test/AspNetCoreActionResultTypization.cs
+++ AspNetCoreActionResultTypizer/AspNetCoreActionResultTypizer.Test/AspNetCoreActionResultTypization.cs
@@ -49,4 +49,141 @@ public class AspNetCoreActionResultTypizerUnitTest
             }
         """, AnalyzerDiagnostic.WithSpan(4, 16, 4, 29));
     }
+
+    [Fact]
+    public async Task OkThatsNotTheBaseControllersOk_IsIgnored()
+    {
+        await VerifyCS.VerifyAnalyzerAsync("""
+            using Microsoft.AspNetCore.Mvc;
+            public class MyController : Controller
+            {
+                public IActionResult MyAction()
+                {
+                    IActionResult Ok(object x) => this.Ok(x);  
+                    var x = "Hello World";
+                    return Ok(x);
+                }
+            }
+        """);
+    }
+
+    [Fact]
+    public async Task CodeFixWorksForSimpleType_NotAsync()
+    {
+        await VerifyCS.VerifyCodeFixAsync("""
+            using Microsoft.AspNetCore.Mvc;
+            public class MyController : Controller
+            {
+                public IActionResult MyAction()
+                {
+                    var x = "Hello World";
+                    return Ok(x);
+                }
+            }
+        """, """
+            using Microsoft.AspNetCore.Mvc;
+            public class MyController : Controller
+            {
+                public ActionResult<string> MyAction()
+                {
+                    var x = "Hello World";
+                    return Ok(x);
+                }
+            }
+        """);
+    }
+    
+    [Fact]
+    public async Task CodeFixWorksForSimpleType_Async()
+    {
+        await VerifyCS.VerifyCodeFixAsync("""
+            using Microsoft.AspNetCore.Mvc;
+            public class MyController : Controller
+            {
+                public async Task<IActionResult> MyAction()
+                {
+                    var x = "Hello World";
+                    return Ok(x);
+                }
+            }
+        """, """
+            using Microsoft.AspNetCore.Mvc;
+            public class MyController : Controller
+            {
+                public async Task<ActionResult<string>> MyAction()
+                {
+                    var x = "Hello World";
+                    return Ok(x);
+                }
+            }
+        """);
+    }
+    
+    [Fact]
+    public async Task CodeFixWorksForGenericType_NotAsync()
+    {
+        await VerifyCS.VerifyCodeFixAsync("""
+            using Microsoft.AspNetCore.Mvc;
+            public class GenericType<T>         
+            {
+                public T Value { get; set; }
+            } 
+            public class MyController : Controller
+            {
+                public IActionResult MyAction()
+                {
+                    var x = new GenericType<string> { Value = "Hello World" };
+                    return Ok(x);
+                }
+            }
+        """, """
+            using Microsoft.AspNetCore.Mvc;
+            public class GenericType<T>
+            {
+                public T Value { get; set; }
+            } 
+            public class MyController : Controller
+            {
+                public ActionResult<GenericType<string>> MyAction()
+                {
+                    var x = new GenericType<string> { Value = "Hello World" };
+                    return Ok(x);
+                }
+            }
+        """);
+    }
+    
+    [Fact]
+    public async Task CodeFixWorksForGenericType_Async()
+    {
+        await VerifyCS.VerifyCodeFixAsync("""
+            using Microsoft.AspNetCore.Mvc;
+            public class GenericType<T>         
+            {
+                public T Value { get; set; }
+            } 
+            public class MyController : Controller
+            {
+                public async Task<IActionResult> MyAction()
+                {
+                    var x = new GenericType<string> { Value = "Hello World" };
+                    return Ok(x);
+                }
+            }
+        """, """
+            using Microsoft.AspNetCore.Mvc;
+            public class GenericType<T>
+            {
+                public T Value { get; set; }
+            } 
+            public class MyController : Controller
+            {
+                public async Task<ActionResult<GenericType<string>>> MyAction()
+                {
+                    var x = new GenericType<string> { Value = "Hello World" };
+                    return Ok(x);
+                }
+            }
+        """);
+    }
 }
